7e501cd0ffc1b6a5ec8b4ff6c9d90f9b098fb5b1
JBayona <jordavids_22@hotmail.com>
2020-12-24 21:23:42 -0600

    Find Maximum In Sliding Window Size K.

--- Arrays/findMaximumInSlidingWindowSizeK.js
+++ Arrays/findMaximumInSlidingWindowSizeK.js
@@ -16,6 +16,47 @@ https://www.educative.io/courses/coderust-hacking-the-coding-interview/k5llE
 */
 
 // Option 1
+// Time O(N)
+let findMaxSlidingWindow = function(arr, windowSize) {
+  var result = [];
+  if(arr.length == 0) {
+    return [];
+  }
+
+  if(windowSize > arr.length) {
+    return [];
+  }
+
+  let dequeue = [];
+  // Find out max for the first window
+  for(let i = 0; i < windowSize; i++) {
+    while(dequeue.length && arr[i] > arr[dequeue[dequeue.length - 1]]) {
+      dequeue.pop();
+    }
+    // Insert the index of the element
+    dequeue.push(i);
+  }
+
+  result.push(arr[dequeue[0]])
+
+  for(let i = windowSize; i < arr.length; i++) {
+    // remove all numbers that are smaller than current number
+    // from the tail of list
+    while (dequeue.length && arr[i] >= arr[dequeue[dequeue.length - 1]]) {
+      dequeue.pop();
+    }
+    //remove first number if it doesn't fall in the window anymore
+    if (dequeue.length > 0 && (dequeue[0] <= i - windowSize)) {
+      dequeue.shift();
+    }
+
+    dequeue.push(i);
+    result.push(arr[dequeue[0]]);
+  }
+  return result;
+};
+
+// Option 2
 // Time O((N - k + 1) * k) = O(n * k)
 let findMaxSlidingWindow = function(arr, window_size) {
   var result = [];
